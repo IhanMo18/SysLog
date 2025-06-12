cd9719cd1b96eba388ae6a79f65f2fa5ac753acf
David Orrego <orregod091@gmail.com>
2025-06-12 13:32:27 -0400

    Add Shared Library

--- SysLog.Api/obj/Debug/net9.0/SysLog.Api.AssemblyInfo.cs
+++ SysLog.Api/obj/Debug/net9.0/SysLog.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysLog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4ba20ade37d6984add9cba4027237ab2a32a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b279d71bd485d0f0aaaa91865634fd2dd687016")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysLog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysLog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
