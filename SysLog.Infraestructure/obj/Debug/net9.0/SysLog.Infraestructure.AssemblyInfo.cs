cafa39a9f7f09ad7e928675571355a86422a5d98
David Orrego <orregod091@gmail.com>
2025-06-12 11:55:07 -0400

    Refactor UI

--- SysLog.Infraestructure/obj/Debug/net9.0/SysLog.Infraestructure.AssemblyInfo.cs
+++ SysLog.Infraestructure/obj/Debug/net9.0/SysLog.Infraestructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysLog.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801ab5566aec34ff790be358522ccef7d7aa2cc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f89ee5a060899cf6ea0f9f2f14fce69cb5d4e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysLog.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysLog.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
