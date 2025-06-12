f9a7f93899dffeb5df876155861d626a14f60144
Ihan <156491555+IhanMo18@users.noreply.github.com>
2025-06-12 10:44:33 -0400

    Fix backup repository query ordering

--- SysLog.Infraestructure/Repositories/BackupFileRepository.cs
+++ SysLog.Infraestructure/Repositories/BackupFileRepository.cs
@@ -1,6 +1,7 @@
 using SysLog.Domine.Interfaces.Repositories;
 using SysLog.Repository.Data;
 using SysLog.Repository.Model;
+using System.IO;
 
 namespace SysLog.Repository.Repositories;
 
@@ -8,8 +9,17 @@ public class BackupFileRepository(BackupDbContext backupDbContext): Repository<B
 {
     public int GetLastBackupFileDayTime()
     {
-       var file = backupDbContext.BackupFile.Last();
-       var dayString = file.FileName[6..];
-       return Convert.ToInt32(dayString);
+        var file = backupDbContext.BackupFile
+            .OrderByDescending(f => f.Id)
+            .FirstOrDefault();
+
+        if (file is null)
+            return 0;
+
+        var name = Path.GetFileNameWithoutExtension(file.FileName);
+        if (name.Length >= 8 && int.TryParse(name.Substring(6, 2), out var day))
+            return day;
+
+        return 0;
     }
 }
