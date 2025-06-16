6567fab6ba5de72cd9bcf22ba5324d5e623a0bf1
Ihan <156491555+IhanMo18@users.noreply.github.com>
2025-06-16 10:49:19 -0400

    feat: add backup log loading

--- SysLog.Domine/Interfaces/Repositories/IBackupFileRepository.cs
+++ SysLog.Domine/Interfaces/Repositories/IBackupFileRepository.cs
@@ -1,8 +1,10 @@
 using SysLog.Repository.Model;
+using System;
 
 namespace SysLog.Domine.Interfaces.Repositories;
 
 public interface IBackupFileRepository : IRepository<BackupFile>
 {
     public int GetLastBackupFileDayTime();
+    Task<BackupFile?> FindByDateAsync(DateTime date);
 }
