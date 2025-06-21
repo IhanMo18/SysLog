eca68354d7bb4ff641aafc51bda5181457e7ca29
David Orrego <orregod091@gmail.com>
2025-06-21 18:59:11 -0400

    Clean artefacts and adding new .gitignore

---
+++ SysLog.Infraestructure/Data/Migrations/20250616232031_IdentityRefactor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SysLog.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityRefactor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
